c7779bf1bc0ed6d1d17c01aaf065093d02d15d4d
Ulyana <93511769+manulya@users.noreply.github.com>
2023-01-18 22:44:29 +0300

    Add files via upload
    ready cv

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("my_btn").style.display = "block";
+    } else {
+        document.getElementById("my_btn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
